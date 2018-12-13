6f49ac38124270107aab34b78b1b5ea57781cbca
Jorge Lazo <m18_jorge@lev.thep.lu.se>
2018-12-13 11:39:03 +0100

    default values set to 0.5

--- general/generate_training_and_validation.py
+++ general/generate_training_and_validation.py
@@ -3,32 +3,34 @@ import random
 import shutil
 
 
-def generate_training_and_validation_sets(training_percentage=0.1):
+def generate_training_and_validation_sets(training_percentage=0.5):
 
-    current_directory = os.getcwd()
-    files_path_positives = "".join([current_directory, '/', 'interesting/'])
-    files_path_negatives = "".join([current_directory, '/', 'not_interesting/'])
+    current_directory = '/home/william/m18_jorge/Desktop/THESIS/DATA/cats_dogs/cats_and_dogs/'
+    files_path_positives = "".join([current_directory, 'cats/'])
+    files_path_negatives = "".join([current_directory, 'dogs/'])
     positive_images = os.listdir(files_path_positives)
     negative_images = os.listdir(files_path_negatives)
 
-    training_dir = '/home/jl/MI_BIBLIOTECA/Escuela/Lund/IV/Thesis/test_data_set/training_vgg1/training/'
-    validation_dir = '/home/jl/MI_BIBLIOTECA/Escuela/Lund/IV/Thesis/test_data_set/training_vgg1/validation/'
+    training_dir = '/home/william/m18_jorge/Desktop/THESIS/DATA/cats_dogs/cats_and_dogs/training/'
+    validation_dir = '/home/william/m18_jorge/Desktop/THESIS/DATA/cats_dogs/cats_and_dogs/validation/'
 
     for count_i, image in enumerate(positive_images):
+        print(count_i,len(positive_images))
         if random.random() <= training_percentage:
-            shutil.copy(files_path_positives + image, "".join([training_dir, 'positives/', image]))
+            shutil.copy(files_path_positives + image, "".join([training_dir, 'cats/', image]))
         else:
-            shutil.copy(files_path_positives + image, "".join([validation_dir, 'positives/', image]))
-
+            shutil.copy(files_path_positives + image, "".join([validation_dir, 'cats/', image]))
+            
     for count_i, image in enumerate(negative_images):
+        print(count_i,len(negative_images))
         if random.random() <= training_percentage:
-            shutil.copy(files_path_negatives + image, "".join([training_dir, 'negatives/', image]))
+            shutil.copy(files_path_negatives + image, "".join([training_dir, 'dogs/', image]))
         else:
-            shutil.copy(files_path_negatives + image, "".join([validation_dir, 'negatives/', image]))
+            shutil.copy(files_path_negatives + image, "".join([validation_dir, 'dogs/', image]))
 
 
 def main():
-    generate_training_and_validation_sets(training_percentage=0.15)
+    generate_training_and_validation_sets(training_percentage=0.6)
 
 
 if __name__ == '__main__':
