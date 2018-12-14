6fdc5f109acd27cadab14420275b003a290279f8
Jorge <j.org.ls@ciencias.unam.mx>
2018-12-14 14:47:53 +0100

    main structure

--- general/roc_curve.py
+++ general/roc_curve.py
@@ -2,7 +2,7 @@ from sklearn.metrics import roc_curve
 import csv
 import matplotlib.pyplot as plt
 from sklearn.metrics import auc
-
+import os
 
 
 def load_predictions(csv_file):
@@ -11,7 +11,7 @@ def load_predictions(csv_file):
     with open(csv_file, 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         for row in reader:
-            labels.append(row[2])
+            labels.append(row[1])
 
             image_name.append(row[0])
     return labels, image_name
@@ -28,46 +28,61 @@ def load_labels(csv_file):
     return labels, image_name
 
 
-y_results, names = load_predictions('Inception_predictions.csv')
-y_2test, names_test = load_labels('Real_values_test.csv')
-y_test = []
-y_pred = []
+def main(predicted, real):
 
-print(len(y_results), len(names))
-print(len(y_2test), len(names_test))
+    y_results, names = load_predictions(predicted)
+    y_2test, names_test = load_labels(real)
 
-for i, name in enumerate(names):
-    for j, other_name in enumerate(names_test):
-        if name == other_name:
-            y_pred.append(float(y_results[i]))
-            y_test.append(int(y_2test[j]))
+    #y_results, names = load_predictions('Inception_predictions.csv')
+    #y_2test, names_test = load_labels('Real_values_test.csv')
+    y_test = []
+    y_pred = []
 
-print(len(y_pred))
-print(len(y_test))
-print(y_pred)
-print(y_test)
-fpr_keras, tpr_keras, thresholds_keras = roc_curve(y_test, y_pred)
+    print(len(y_results), len(names))
+    print(len(y_2test), len(names_test))
 
-auc_keras = auc(fpr_keras, tpr_keras)
+    for i, name in enumerate(names):
+        for j, other_name in enumerate(names_test):
+            if name == other_name:
+                y_pred.append(float(y_results[i]))
+                y_test.append(int(y_2test[j]))
 
-plt.figure()
-plt.plot([0, 1], [0, 1], 'k--')
-plt.plot(fpr_keras, tpr_keras, label='Keras (area = {:.3f})'.format(auc_keras))
-#plt.plot(fpr_rf, tpr_rf, label='RF (area = {:.3f})'.format(auc_rf))
-plt.xlabel('False positive rate')
-plt.ylabel('True positive rate')
-plt.title('ROC curve')
-plt.legend(loc='best')
+    print(len(y_pred))
+    print(len(y_test))
+    fpr_keras, tpr_keras, thresholds_keras = roc_curve(y_test, y_pred)
 
-# Zoom in view of the upper left corner.
-plt.figure()
-plt.xlim(0, 0.2)
-plt.ylim(0.8, 1)
-plt.plot([0, 1], [0, 1], 'k--')
-plt.plot(fpr_keras, tpr_keras, label='Keras (area = {:.3f})'.format(auc_keras))
-#plt.plot(fpr_rf, tpr_rf, label='RF (area = {:.3f})'.format(auc_rf))
-plt.xlabel('False positive rate')
-plt.ylabel('True positive rate')
-plt.title('ROC curve (zoomed in at top left)')
-plt.legend(loc='best')
-plt.show()
+    auc_keras = auc(fpr_keras, tpr_keras)
+
+    plt.figure()
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.plot(fpr_keras, tpr_keras, label='Keras (area = {:.3f})'.format(auc_keras))
+    #plt.plot(fpr_rf, tpr_rf, label='RF (area = {:.3f})'.format(auc_rf))
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.title('ROC curve')
+    plt.legend(loc='best')
+
+    # Zoom in view of the upper left corner.
+    """plt.figure()
+    plt.xlim(0, 0.2)
+    plt.ylim(0.8, 1)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.plot(fpr_keras, tpr_keras, label='Keras (area = {:.3f})'.format(auc_keras))
+    #plt.plot(fpr_rf, tpr_rf, label='RF (area = {:.3f})'.format(auc_rf))
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.title('ROC curve (zoomed in at top left)')
+    plt.legend(loc='best')"""
+    plt.show()
+
+if __name__ == "__main__":
+
+    predicted = 'results/Inception_predictions_20181213-20h12m_l2_0.06.csv'
+    real = 'real_values/Real_values_validation.csv'
+
+    files = os.listdir('results')
+    for file in files:
+        print(file[-7:])
+        main('results/'+file, real)
+
+
