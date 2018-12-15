362b47b8de2c9e919125761c70b9f558aefd6fa8
Jorge <j.org.ls@ciencias.unam.mx>
2018-12-15 18:09:49 +0100

    program with the general functions used

---
+++ general/general_functions.py
@@ -0,0 +1,23 @@
+import csv
+
+def load_predictions(csv_file):
+    labels = []
+    image_name = []
+    with open(csv_file, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            labels.append(row[1])
+
+            image_name.append(row[0])
+    return labels, image_name
+
+
+def load_labels(csv_file):
+    labels = []
+    image_name = []
+    with open(csv_file, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            labels.append(float(row[0]))
+            image_name.append(row[1])
+    return labels, image_name
