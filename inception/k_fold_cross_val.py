3145b38e2f4fb86e5a4836a250e384d1ec79db3f
Jorge Lazo <m18_jorge@fakiren.thep.lu.se>
2019-01-22 16:34:47 +0100

    added step to chekc in the validation set

--- inception/k_fold_cross_val.py
+++ inception/k_fold_cross_val.py
@@ -79,8 +79,7 @@ def calculate_auc_and_roc(predicted, real, plot=False):
     return auc_keras
 
 
-def main(train_data_dir, validation_data_dir, test_data_dir_1, test_data_dir_2, value=0.1):
-    
+def main(train_data_dir, validation_data_dir, test_data_dir_1, test_data_dir_2, idx=0, value=0.1):
     # ------------------------directories of the datasets -------------------------------
     
     #train_data_dir = '/home/william/m18_jorge/Desktop/THESIS/DATA/transfer_learning_training/training/'
@@ -99,7 +98,8 @@ def main(train_data_dir, validation_data_dir, test_data_dir_1, test_data_dir_2, 
     
     ROWS = 139
     COLS = 139
-    
+    name_val_dir = validation_data_dir[-3:]
+
     # --------------------- Image Data Generator-------------------
     train_idg = ImageDataGenerator(preprocessing_function=preprocess_input)
     val_idg = ImageDataGenerator(preprocessing_function=preprocess_input)
@@ -123,7 +123,6 @@ def main(train_data_dir, validation_data_dir, test_data_dir_1, test_data_dir_2, 
                                         batch_size = 200)   
                                         
     lenv_test2 = len(os.listdir(test_data_dir_2))   
-    print('len', lenv_test2)                                  
     test_gen2 = test_idg2.flow_from_directory(test_data_dir_2, 
                                         target_size=(ROWS, COLS), 
                                         shuffle=False,
@@ -142,7 +141,7 @@ def main(train_data_dir, validation_data_dir, test_data_dir_1, test_data_dir_2, 
     add_model.add(base_model)
     add_model.add(GlobalAveragePooling2D())
     #add_model.add(Dropout(value))
-    add_model.add(Dense(1024, activation='relu'))
+    #add_model.add(Dense(1024, activation='relu'))
     add_model.add(Dense(1024, activation='relu', kernel_regularizer=regularizers.l2(value)))
     
     add_model.add(Dense(nclass, activation='softmax'))
@@ -185,10 +184,10 @@ def main(train_data_dir, validation_data_dir, test_data_dir_1, test_data_dir_2, 
                                           batch_size = 100)
     
     evaluation = model.evaluate_generator(validation_gen, verbose = True, steps=10)
-    print(evaluation)
+    print(evaluation, 'validation data')
     
     evaluation_0 = model.evaluate_generator(test_gen, verbose = True, steps=1)
-    print(evaluation_0)
+    print(evaluation_0, 'RGB data')
     
     ###-----------------------lets make predictions-------------------
     predicts = model.predict_generator(test_gen, verbose = True, steps=1)
@@ -212,7 +211,7 @@ def main(train_data_dir, validation_data_dir, test_data_dir_1, test_data_dir_2, 
     df['class_1'] = x_0
     df['class_2'] = x_1
     df['over all'] = predicts
-    name_save_predictions_1 = ''.join(['predictions_rgb_keras2_', str(value),'_.csv'])   
+    name_save_predictions_1 = ''.join(['predictions_rgb_keras2_', str(idx), '_', str(value),  '_.csv'])   
     df.to_csv(name_save_predictions_1, index=False)
     
 
@@ -246,8 +245,38 @@ def main(train_data_dir, validation_data_dir, test_data_dir_1, test_data_dir_2, 
     df['class_1'] = x_0
     df['class_2'] = x_1
     df['over all'] = predicts2
-    name_save_predictions_2 = ''.join(['predictions_IR_keras2_', str(value), '_.csv'])
+    name_save_predictions_2 = ''.join(['predictions_IR_keras2_', str(idx), '_', str(value), '_.csv'])
     df.to_csv(name_save_predictions_2, index=False)
+    
+    # -------------------------predictions on the validation set --------------------------
+    test_idg2 = ImageDataGenerator(preprocessing_function=preprocess_input)
+    va_gen2 = test_idg2.flow_from_directory(validation_data_dir, 
+                                  target_size=(ROWS, COLS), 
+                                   shuffle=False,
+                                   batch_size = 100) 
+                                   
+    predict3 = model.predict_generator(va_gen2, verbose = True, steps=40)
+    
+    #print(len(predicts))
+    #print(predicts[:270])
+    #print('second part')
+    #print(predicts[270:])
+    x_0 = [x[0] for x in predict3]
+    x_1 = [x[1] for x in predict3]
+    names = [os.path.basename(x) for x in va_gen2.filenames[:4000]]
+    print(len(x_0), len(names))
+    
+    predict3 = np.argmax(predict3, axis=1)
+    label_index = {v: k for k,v in va_gen2.class_indices.items()}
+    predict3 = [label_index[p] for p in predict3]
+    
+    df = pd.DataFrame(columns=['class_1', 'class_2', 'fname', 'over all'])
+    df['fname'] = names
+    df['class_1'] = x_0
+    df['class_2'] = x_1
+    df['over all'] = predict3
+    name_save_predictions_3 = ''.join(['predictions_keras2_', name_val_dir, '_', str(idx), '_', str(value), '_.csv'])
+    df.to_csv(name_save_predictions_3, index=False)
     
     # -----------now lets calculate the AUC---------------------------------
     
@@ -258,7 +287,10 @@ def main(train_data_dir, validation_data_dir, test_data_dir_1, test_data_dir_2, 
     real_val = '/home/william/m18_jorge/Desktop/THESIS/DATA/real_values/Real_values_case4_IR.csv'
     auch_1 = calculate_auc_and_roc(name_save_predictions_2, real_val)
     print(auch_1, 'IR')
-
+    
+    real_val = ''.join(['/home/william/m18_jorge/Desktop/THESIS/DATA/real_values/', name_val_dir,'.csv'])
+    auch_2 = calculate_auc_and_roc(name_save_predictions_3, real_val)
+    print(auch_2, name_val_dir)
 
 def copy_files(initial_dir, final_dir):
     subfolders_initial = os.listdir(initial_dir) 
@@ -278,13 +310,15 @@ if __name__ == "__main__":
     folders = os.listdir(initial_dir)
     test_dir_rgb = '/home/william/m18_jorge/Desktop/THESIS/DATA/case4_test/rgb/'
     test_dir_ir = '/home/william/m18_jorge/Desktop/THESIS/DATA/case4_test/IR/'
-    posible_values = [0.8, 0.8, 0.8, 0.8]    
+    #posible_values = [[0.8, 0.01, 0.3], [0.4, 1.2, 0.008], [0.1, 0.9, 0.001] , [0.005, 0.5, 1.5]]    
+    #posible_values = [[0.01], [0.01], [0.01] , [0.01]]   # you used this result when you wanted to see the behaviour in general
+    posible_values = [[0.08], [0.08], [0.08] , [0.08]]
     train_dir = '/home/william/m18_jorge/Desktop/THESIS/DATA/tem_train/'
     if (os.path.isdir(train_dir)):
         shutil.rmtree(train_dir)
     print(folders)
     number_folders = list(np.arange(0, len(folders), 1))
-    
+
     for num, subfolder in enumerate(folders):
         number_folders.remove(number_folders.index(num))
         val_dir = ''.join([initial_dir, subfolder])
@@ -301,8 +335,8 @@ if __name__ == "__main__":
             print(folders[remaining])
             check_folder = ''.join([initial_dir, folders[remaining], '/'])  
             copy_files(check_folder, train_dir)
-                        
-        main(train_dir, val_dir, test_dir_rgb, test_dir_ir, posible_values[num])
+        for j in range(len(posible_values[num])):
+            main(train_dir, val_dir, test_dir_rgb, test_dir_ir, num, posible_values[num][j])
         shutil.rmtree(train_dir)
         number_folders = list(np.arange(0, len(folders), 1))
         
